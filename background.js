f6329377e565e2eeb41a5a2154cc915b15921955
Naveen Kharwar <naveenkharwar21@protonmail.com>
2023-08-19 09:24:16 +0530

    :fire:: no need

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.action === "searchDivs") {
-	  chrome.tabs.executeScript(null, { file: "content.js" });
-	}
-  });
