e25f480415d52fce3265935c3a909fbdb667caaf
Naveen Kharwar <naveenkharwar21@protonmail.com>
2023-08-19 09:40:57 +0530

    🙈: Teacher says always make you code readable

--- content.js
+++ content.js
@@ -1,30 +1,35 @@
-// Function to blur the elements
-function blurElements(elements) {
-	elements.forEach(div => {
-	//   div.style.filter = 'blur(5px)';
-	//   div.style.pointerEvents = 'none';
-	// Feel free to change the styles
-	  div.style.display = 'none';
-	});
-  }
+function hideElements(elements) {
+  elements.forEach((element) => {
+    // Customize the style to hide elements
+    element.style.display = "none";
+  });
+}
 
-  // Observe changes in the DOM
-  const observer = new MutationObserver(mutations => {
-	const targetElements = document.querySelectorAll('ytd-rich-item-renderer.style-scope.ytd-rich-grid-row');
+// Observe changes in the DOM
+const observer = new MutationObserver((mutations) => {
+  const targetElements = document.querySelectorAll(
+    "ytd-rich-item-renderer.style-scope.ytd-rich-grid-row"
+  );
 
-	const elvishYadavElements = Array.from(targetElements).filter(element => {
-	  return element.textContent.includes('Elvish Yadav');
-	});
-
-	blurElements(elvishYadavElements);
+  const filteredElements = Array.from(targetElements).filter((element) => {
+    // Customize the condition to filter elements
+    return element.textContent.includes("Elvish Yadav");
   });
 
-  // Start observing the document
-  observer.observe(document, { childList: true, subtree: true });
+  hideElements(filteredElements);
+});
 
-  // Apply the blur initially
-  const initialElvishYadavElements = Array.from(document.querySelectorAll('ytd-rich-item-renderer.style-scope.ytd-rich-grid-row')).filter(element => {
-	return element.textContent.includes('Elvish Yadav');
-  });
+// Start observing the document
+observer.observe(document, { childList: true, subtree: true });
 
-  blurElements(initialElvishYadavElements);
+// Apply the hiding initially
+const initialElements = Array.from(
+  document.querySelectorAll(
+    "ytd-rich-item-renderer.style-scope.ytd-rich-grid-row"
+  )
+).filter((element) => {
+  // Customize the condition for initial filtering
+  return element.textContent.includes("Elvish Yadav");
+});
+
+hideElements(initialElements);
